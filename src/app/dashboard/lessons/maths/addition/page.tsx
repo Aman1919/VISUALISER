d37403aefa0f7d6170105a3e224eb3407bb0d259
Aman singh <amansingh33849@gmail.com>
2025-04-02 01:53:26 +0530

    changes

---
+++ src/app/dashboard/lessons/maths/addition/page.tsx
@@ -0,0 +1,3 @@
+export default function Additions() {   
+    return <div>Maths Additions</div>
+}
--- src/app/lesson/page.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Lesson() {
-    return (
-      <div>
-  Lesson
-      </div>
-    );
-  }
-  
